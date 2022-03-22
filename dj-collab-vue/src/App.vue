8c1f8021ffcb55b9a5f7223f2de54771dee6f450
Siora van Weperen <428439@student.fontys.nl>
2022-03-22 10:57:39 +0100

    DC-1 eerste opzet frontend

--- dj-collab-vue/src/App.vue
+++ dj-collab-vue/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PartyList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PartyList from './components/PartyList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PartyList
   }
 }
 </script>
---
+++ dj-collab-vue/src/components/PartyList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <li v-for="party in parties" v-bind:key="party.id">{{ party.name }}</li>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'PartyList',
+  props: {
+  },
+  data() {
+    return {
+      parties: [{id: 0}, {}],
+    }
+  },
+  mounted() {
+    this.getParty();
+  },
+  methods: {
+    async getParty() {
+    var parties = await axios.get('https://localhost:8081/party');
+    this.parties.push(parties);
+    }
+  }
+};
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
