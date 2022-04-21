07ef8700914228206669af3347cbf01481a9765e
Siora van Weperen <428439@student.fontys.nl>
2022-04-21 16:30:01 +0200

    getting and filling users in app for better continuity

--- dj-collab-vue/src/App.vue
+++ dj-collab-vue/src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
     <AddUserForm />
-    <UserList />
+    <UserList v-bind:users="users" />
   </div>
 </template>
 
 <script>
 import UserList from './components/UserList.vue';
 import AddUserForm from './components/AddUserForm.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     AddUserForm,
     UserList
+  },
+  data() {
+    return {
+      users: [],
+    }
+  },  
+  mounted() {
+    this.getUsers();
+  },
+  methods: {
+    async getUsers() {
+    var response = await axios.get('http://localhost:8081/user/all');
+    var users = await response.data;
+    this.users = users;
+    }
   }
 }
 </script>
--- dj-collab-vue/src/components/UserList.vue
+++ dj-collab-vue/src/components/UserList.vue
@@ -9,7 +9,6 @@
 </template>
 
 <script>
-import axios from 'axios';
 import vaadinGrid from '@vaadin/grid';
 
 export default {
@@ -18,22 +17,14 @@ export default {
     vaadinGrid,
   },
   props: {
+    users: Array,
   },
   data() {
     return {
-      users: [{id: 0}, {}],
+      
     }
   },
-  mounted() {
-    this.getUsers();
-  },
-  methods: {
-    async getUsers() {
-    var response = await axios.get('http://localhost:8081/user/all');
-    var users = await response.data;
-    this.users = users;
-    }
-  }
+  
 };
 
 
